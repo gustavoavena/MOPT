751e182280a2ec90d94b3cebaf672dcd083d5124
Filipe Marques <filipe@Filipes-MacBook-Pro.local>
2017-03-30 18:21:34 -0300

    table views

--- MOPT/MOPT/ViewController.swift
+++ MOPT/MOPT/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var meetingName: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
