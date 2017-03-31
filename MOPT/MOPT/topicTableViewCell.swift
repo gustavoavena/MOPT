2c6348d3b1259a3999a488a85a6ece6f354d3271
Filipe Marques <filipe@Filipes-MacBook-Pro.local>
2017-03-31 18:40:32 -0300

    sotryboard substancial changes

---
+++ MOPT/MOPT/topicTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  topicTableViewCell.swift
+//  MOPT
+//
+//  Created by Filipe Marques on 31/03/17.
+//  Copyright © 2017 Gustavo Avena. All rights reserved.
+//
+
+import UIKit
+
+class topicTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
